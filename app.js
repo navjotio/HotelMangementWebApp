e700813271d59dddf38bb602ac4845e3b1e6c8c1
NavDevv <navjot200284@gmail.com>
2023-08-02 14:03:25 -0500

    added the hidden password feature

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+//Show password feature 
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#id_password');
+
+togglePassword.addEventListener('click', function (e) {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
