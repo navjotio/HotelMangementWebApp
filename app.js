9fa827d85daad68afa137f83e9969fad782bd775
NavDevv <navjot200284@gmail.com>
2023-08-05 21:20:18 -0500

    LoginButtonChanges

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 'use strict';
-
 //Show password feature 
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#id_password');
@@ -10,3 +9,22 @@ togglePassword.addEventListener('click', function (e) {
     // toggle the eye slash icon
     this.classList.toggle('fa-eye-slash');
 });
+
+// Setting the password the login page 
+const username = document.getElementById('Uname');
+const password1 = document.getElementById('id_password');
+const form = document.getElementById('LoginForm')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (username.value === "admin" && password1.value === '1234') {
+        location.href = "sections.html";
+        alert(`You are logged in ${username}`)
+    }
+    else {
+        document.location.href = document.location.href;
+    }
+})
+
+
