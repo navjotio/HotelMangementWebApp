e7d4ff1e6f25509a68e8c90ccf7d695c9df5b644
NavDevv <navjot200284@gmail.com>
2023-08-08 20:34:18 -0500

    Made sections Functional

---
+++ sections.js
@@ -0,0 +1,12 @@
+// Code for the Sections Page 
+function validate() {
+    if (true) {
+        location.href = "Management.html"
+    }
+}
+
+function validate1() {
+    if (true) {
+        location.href = "Bookings.html"
+    }
+}
